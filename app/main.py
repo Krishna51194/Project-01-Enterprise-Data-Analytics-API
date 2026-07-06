a9646f4feb1caae825eac92e559597ae7cea6396
krishna51194 <krishna.gudladana999@gmail.com>
2026-07-05 21:38:27 -0600

    Add API metadata and health check endpoint

--- app/main.py
+++ app/main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
-app = FastAPI() 
+
+
+app = FastAPI(
+    title = "Enterprise Data Analytics API",
+    description = "A professional API for data analytics, reorting, and dataset processing",
+    version = "1.0.0"
+) 
+
+
 @app.get("/") 
 def home():
     return {
-        "message" : "Welcome to Enterprise Data Analytuics API"
+        "message" : "Welcome to Enterprise Data Analytics API",
+        "status" : "running",
+        "version" : "1.0.0"
+    }
+    
+
+@app.get("/health")
+def health_check():
+    return {
+        "status" : "healthy",
+        "service" : "Enterrisse Data Analytics API"
     }
