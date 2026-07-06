09402a700d81bb55b8261442a705df105c50b4e6
krishna51194 <krishna.gudladana999@gmail.com>
2026-07-05 22:22:51 -0600

    Add dataset schema and POST endpoint

--- app/routers/datasets.py
+++ app/routers/datasets.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from app.schemas.dataset import Dataset
 router = APIRouter()
 
 @router.get("/datasets/{dataset_id}")
@@ -10,10 +11,18 @@ def get_dataset(dataset_id : int):
         "message" : f"Dataset {dataset_id} details will be shown here"
     }
 
+
 @router.get("/datasets")
 def list_datasets(limit : int = 10, category : str | None = None):
     return {
         "limit" : limit,
         "category" : category,
         "message" : "Dataset list will be shown here"
+    }
+
+@router.post("/datasets")
+def create_dataset(dataset : Dataset):
+    return {
+        "message" : "Dataset created successfully",
+        "dataset" : dataset
     }
